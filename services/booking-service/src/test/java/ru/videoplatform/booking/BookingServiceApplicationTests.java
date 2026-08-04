ba436a36dc8b9c2c663b8723cc5002d9d7ce9ec2
JeDiFoXxX <serjik1993991@gmail.com>
2026-08-04 18:53:18 +0300

    feat(booking): bootstrap module with jpa, liquibase and testcontainers

--- services/booking-service/src/test/java/ru/videoplatform/booking/BookingServiceApplicationTests.java
+++ services/booking-service/src/test/java/ru/videoplatform/booking/BookingServiceApplicationTests.java
@@ -3,9 +3,11 @@ package ru.videoplatform.booking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class BookingServiceApplicationTests {
 
 	@Test
