34186683f22effc3fca37881beba5d470c0369ca
JeDiFoXxX <serjik1993991@gmail.com>
2026-08-16 13:21:35 +0300

    feat(bot): init module and configure telegram webhook

---
+++ services/bot-service/src/main/java/ru/videoplatform/bot/BotServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.videoplatform.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ru.videoplatform.bot.BotServiceApplication.class, args);
+    }
+
+}
---
+++ services/bot-service/src/test/java/ru/videoplatform/bot/BotServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package ru.videoplatform.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BotServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
