479bca5e379d32dda3d830716b70c08847a2e3cb
JeDiFoXxX <serjik1993991@gmail.com>
2026-09-17 19:00:38 +0300

    refactor(api-gateway): refactor telegram auth filter logic, property configurations and ApiGatewayApplicationTests

--- services/api-gateway/src/test/java/ru/videoplatform/apigateway/ApiGatewayApplicationTests.java
+++ services/api-gateway/src/test/java/ru/videoplatform/apigateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,6 @@ package ru.videoplatform.apigateway;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @Disabled
 @SpringBootTest
